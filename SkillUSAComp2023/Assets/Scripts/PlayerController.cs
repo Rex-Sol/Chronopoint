33a3558dd613abf029be1483aa27a3f8ae05517a
Isaac Holt <isaac.holt@s.capregboces.org>
2024-01-31 09:58:30 -0500

    Scripted health bar and added crosshairs

--- SkillUSAComp2023/Assets/Scripts/PlayerController.cs
+++ SkillUSAComp2023/Assets/Scripts/PlayerController.cs
@@ -1,18 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float baseDamage = 8;
     [SerializeField] private float trueDamage;
     [SerializeField] private float baseHealth = 150;
-    [SerializeField] private float trueHealth;
+    public float trueHealth;
+    public float currentHealth = 150;
     private float numOfDinoMeat;
     private float numOfRosary;
     private float numOfVinyl;
     private float numOfBigMax;
 
+    [SerializeField] private Scrollbar healthSlide;
     // Start is called before the first frame update
     void Start()
     {
@@ -23,8 +26,10 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         baseDamage = baseDamage + 0.0005f;
-        baseHealth = baseHealth + (0.0005f * 30f);
-        trueHealth = baseHealth + (numOfDinoMeat * 20f) + (numOfRosary * 30f) + (numOfBigMax *40f);
+        baseHealth = baseHealth + (0.0005f);
+        trueHealth = baseHealth + (numOfDinoMeat * 20f) + (numOfRosary * 30f) + (numOfBigMax * 40f);
         trueDamage = baseDamage + (numOfVinyl * (.5f * baseDamage));
+        healthSlide.size = currentHealth / trueHealth;
     }
 }
+
